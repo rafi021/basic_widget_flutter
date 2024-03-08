995a9e3e7531a986e95b1377739448935eb134f6
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-03-08 14:20:02 +0600

    Show Image from Network

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const url ="https://cdn.pixabay.com/photo/2021/07/20/14/59/iron-man-6480952_1280.jpg";
 void main() {
   runApp(
       MaterialApp(
@@ -8,13 +9,7 @@ void main() {
             title: Text('Home Page'),
           ),
           body: Center(
-            child: Text(
-              'Hello Flutter',
-              style: TextStyle(
-                fontSize: 24.0
-              ),
-              textDirection:  TextDirection.ltr,
-            ),
+            child: Image.network(url,width: 400,height: 400,fit: BoxFit.cover,)
           ),
         ),
       )
